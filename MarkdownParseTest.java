dbc7119fd07f63c3e3f4197aa5dfc738a184961a
rdxu1688 <ryx002@ucsd.edu>
2022-02-02 19:58:01 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -28,7 +28,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks() throws IOException {
-        Path fileName = Path.of("C:/Users/arjun/Downloads/Work/School/Year 1/Winter 2022/CSE 15L/Labs/Lab 3, 4, 5/markdown-parse/assets/test-file.md");
+        Path fileName = Path.of("test-file.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> realLinks = List.of("https://something.com", "some-page.html");
@@ -37,7 +37,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinksBreaking() throws IOException {
-        Path fileName = Path.of("C:/Users/arjun/Downloads/Work/School/Year 1/Winter 2022/CSE 15L/Labs/Lab 3, 4, 5/markdown-parse/assets/breaking_tests.md");
+        Path fileName = Path.of("breaking_tests.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> realLinks = List.of("https://google.com/(())a()shd", "https://something()test()");
@@ -46,7 +46,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks2() throws IOException {
-        Path fileName = Path.of("C:/Users/arjun/Downloads/Work/School/Year 1/Winter 2022/CSE 15L/Labs/Lab 3, 4, 5/markdown-parse/assets/test-file2.md");
+        Path fileName = Path.of("test-file2.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> realLinks = List.of("https://something.com", "some-page.html");
@@ -55,7 +55,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks3() throws IOException {
-        Path fileName = Path.of("C:/Users/arjun/Downloads/Work/School/Year 1/Winter 2022/CSE 15L/Labs/Lab 3, 4, 5/markdown-parse/assets/test-file3.md");
+        Path fileName = Path.of("test-file3.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> realLinks = List.of();
@@ -64,7 +64,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks4() throws IOException {
-        Path fileName = Path.of("C:/Users/arjun/Downloads/Work/School/Year 1/Winter 2022/CSE 15L/Labs/Lab 3, 4, 5/markdown-parse/assets/test-file4.md");
+        Path fileName = Path.of("test-file4.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> realLinks = List.of();
@@ -73,7 +73,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks5() throws IOException {
-        Path fileName = Path.of("C:/Users/arjun/Downloads/Work/School/Year 1/Winter 2022/CSE 15L/Labs/Lab 3, 4, 5/markdown-parse/assets/test-file5.md");
+        Path fileName = Path.of("test-file5.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> realLinks = List.of();
@@ -82,7 +82,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks6() throws IOException {
-        Path fileName = Path.of("C:/Users/arjun/Downloads/Work/School/Year 1/Winter 2022/CSE 15L/Labs/Lab 3, 4, 5/markdown-parse/assets/test-file6.md");
+        Path fileName = Path.of("test-file6.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> realLinks = List.of();
@@ -91,7 +91,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks7() throws IOException {
-        Path fileName = Path.of("C:/Users/arjun/Downloads/Work/School/Year 1/Winter 2022/CSE 15L/Labs/Lab 3, 4, 5/markdown-parse/assets/test-file7.md");
+        Path fileName = Path.of("test-file7.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> realLinks = List.of();
@@ -100,7 +100,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks8() throws IOException {
-        Path fileName = Path.of("C:/Users/arjun/Downloads/Work/School/Year 1/Winter 2022/CSE 15L/Labs/Lab 3, 4, 5/markdown-parse/assets/test-file8.md");
+        Path fileName = Path.of("test-file8.md");
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         List<String> realLinks = List.of("a link on the first line");
